a15f85c624941816952f75bd942770be4523c38d
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2023-09-23 21:28:58 +0900

    boot_01

---
+++ boot_01_빈생성/src/main/java/com/exam/dao/DeptDAO.java
@@ -0,0 +1,12 @@
+package com.exam.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DeptDAO {
+
+	public DeptDAO() {
+		System.out.println("DeptDAO 생성");
+	}
+
+}
---
+++ boot_01_빈생성/src/main/java/com/exam/service/DeptServiceImpl.java
@@ -0,0 +1,12 @@
+package com.exam.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeptServiceImpl {
+
+	public DeptServiceImpl() {
+		System.out.println("DeptServiceImpl 생성");
+	}
+
+}
